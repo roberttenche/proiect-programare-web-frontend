081ea3ad6da5ac11beef2ccf4662791d7c6dba73
Tenche Robert <teencherobert@gmail.com>
2023-04-05 07:32:53 +0300

    mock display fix

--- src/app/services/surgeon.service.ts
+++ src/app/services/surgeon.service.ts
@@ -26,9 +26,9 @@ export class SurgeonService {
     else
     {
       let srgList: Surgeon[] = [
-        { id: 0, name: "Salahid Dahari", title: "Head Surgeon", rating: 10, description: "Best surgeon", image: "https://familydoctor.org/wp-content/uploads/2018/02/41808433_l.jpg" },
-        { id: 1, name: "George Miller", title: "Senior Surgeon", rating: 8, description: "Almost best surgeon", image: "https://img.freepik.com/free-photo/attractive-young-male-nutriologist-lab-coat-smiling-against-white-background_662251-2960.jpg?w=2000" },
-        { id: 2, name: "Susan", title: "Junior Surgeon", rating: 5, description: "Bad surgeon", image: "https://media.istockphoto.com/id/1189304032/photo/doctor-holding-digital-tablet-at-meeting-room.jpg?s=612x612&w=0&k=20&c=RtQn8w_vhzGYbflSa1B5ea9Ji70O8wHpSgGBSh0anUg=" }
+        { id: 0, name: "Salahid Dahari", title: "Head Surgeon", rating: 5, description: "Best surgeon", image: "https://familydoctor.org/wp-content/uploads/2018/02/41808433_l.jpg" },
+        { id: 1, name: "George Miller", title: "Senior Surgeon", rating: 4, description: "Almost best surgeon", image: "https://img.freepik.com/free-photo/attractive-young-male-nutriologist-lab-coat-smiling-against-white-background_662251-2960.jpg?w=2000" },
+        { id: 2, name: "Susan", title: "Junior Surgeon", rating: 2, description: "Bad surgeon", image: "https://media.istockphoto.com/id/1189304032/photo/doctor-holding-digital-tablet-at-meeting-room.jpg?s=612x612&w=0&k=20&c=RtQn8w_vhzGYbflSa1B5ea9Ji70O8wHpSgGBSh0anUg=" }
       ]
       return of(srgList)
     }
